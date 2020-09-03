4c6d15f03ca92172eed11681bdd35850bd168bf6
Lance Watanabe <phifertoo@Lances-MacBook-Pro.local>
2020-09-02 17:17:49 -0700

    first

---
+++ src/components/Todos.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- need unique value -->
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem.vue";
+
+export default {
+  // name of component
+  name: "Todos",
+  components: {
+    //   components contained in the this component
+    TodoItem,
+  },
+  //   props received from parent component
+  props: ["todos"],
+};
+</script>
+
+<style scoped>
+</style>
